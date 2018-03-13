c645d6bf3dd9baf538b5b5e3dfd67229341186b9
Fumiya Shigemitsu <gyro.scaremonger@gmail.com>
2018-03-14 02:32:56 +0900

    file.c: Enable to write the file that has already been provided.
    "echo "Hello" >> TEST" is success.

--- file.c
+++ file.c
@@ -39,7 +39,36 @@ static ssize_t sifs_file_read(struct file *fp, char __user *buf, size_t len, lof
 
 static ssize_t sifs_file_write(struct file *fp, const char __user *buf, size_t len, loff_t *ppos)
 {
-	return 0;
+	struct sifs_inode *si_inode;
+	struct inode *inode;
+	struct buffer_head *bh;
+	struct super_block *sb;
+	struct sifs_sb *si_sb;
+	char *buffer;
+
+	sb = fp->f_path.dentry->d_inode->i_sb;
+	si_sb = SIFS_SUPER(sb);
+	inode = fp->f_path.dentry->d_inode;
+	si_inode = SIFS_INODE(inode);
+
+	bh = sb_bread(sb, si_inode->data_block_number);
+	buffer = (char *)bh->b_data;
+
+	buffer += si_inode->file_size;
+
+	if (copy_from_user(buffer, buf, len)) {
+		brelse(bh);
+		printk("Unable to copy file contents from userspace\n");
+		return -EFAULT;
+	}
+
+	mark_buffer_dirty(bh);
+	sync_dirty_buffer(bh);
+	brelse(bh);
+
+	si_inode->file_size += len;
+
+	return len;
 }
 
 const struct file_operations sifs_file_ops = {
